7233b8c94f99363842b431d91075238bcf552751
Celerium-Ce <mohammad.umardarkonme@gmail.com>
2025-03-16 22:51:51 +0530

    fixing broken website

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-const isProd = process.env.NODE_ENV === 'production';
-
-export default {
-  output: 'export', // Ensures static export for GitHub Pages
-  assetPrefix: isProd ? '/celerium-ce/' : '',
-  images: {
-    unoptimized: true, // Required for GitHub Pages
-  },
-};
 
 
+export default nextConfig;
